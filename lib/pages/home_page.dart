f75736fc72652277e897ced8f60de6824add80c1
ianokt0 <ian.gutho16@gmail.com>
2024-07-23 23:07:17 +0700

    update 23 july 2024

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/components/dialog_box.dart';
 import 'package:flutter_course/utils/todo_tile.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // text controller
+  final _controller = TextEditingController();
+  // list of todo tasks
+  List toDoList = [
+    ["Make Tutorial", false],
+    ["Make Love", false],
+  ];
+
+  // checkbox was tapped
+  void checkBoxChanged(bool? value, index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  // save new task
+  void saveNewTask() {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+  // create new task
+  void createNewTask() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return DialogBox(
+            controller: _controller,
+            onSave: saveNewTask,
+            onCancel: () => Navigator.of(context).pop(),
+          );
+        });
+  }
+
+  // delete task
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+  // 1:41:28
 
   @override
   Widget build(BuildContext context) {
@@ -12,15 +63,24 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.yellow,
         title: Text("TO DO"),
         elevation: 0,
+        centerTitle: true,
       ),
-      body: ListView(
-        children: [
-          ToDoTile(
-            taskName: "Make Tutorial",
-            taskCompleted: true,
-            onChanged: (p0) {},
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.yellow,
+        shape: CircleBorder(),
+        child: Icon(Icons.add),
+        onPressed: createNewTask,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+            deleteFunction: (context) => deleteTask(index),
+          );
+        },
       ),
     );
   }
